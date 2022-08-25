9601c6dddaadbb1f581180d902743e87204ec220
zhuchaojing <95963780+zhuchaojing@users.noreply.github.com>
2022-08-25 15:23:01 +0800

    Add files via upload

---
+++ src/main/java/cn/ofpp/Application.java
@@ -0,0 +1,38 @@
+package cn.ofpp;
+
+import cn.ofpp.core.BoyFriend;
+import cn.ofpp.core.GirlFriend;
+import cn.ofpp.core.MessageFactory;
+import cn.ofpp.core.Wx;
+
+/**
+ * 启动类
+ *
+ * 这个理论上只能用测试号 正式的号 个人认证是不支持模板消息的 企业认证的又必须使用微信模板库里的模板 只有测试的可以自定义模板内容
+ * <a href="https://mp.weixin.qq.com/debug/cgi-bin/sandboxinfo?action=showinfo&t=sandbox/index">申请公众号测试应用地址</a>
+ *
+ * @author DokiYolo
+ * Date 2022-08-22
+ */
+public class Application {
+
+    /**
+     * <li>创建配置</li>
+     * <li>创建几个 男/女 朋友</li>
+     * <li>发消息</li>
+     */
+    public static void main(String[] args) {
+        // load and init
+        Bootstrap.init();
+
+        // -----------------  以下为演示数据  ------------------------
+
+        // new 一个 女友
+        GirlFriend girlFriend = new GirlFriend("李玲尔",
+                "北京市", "北京市", "1996-08-08", "2011-04-16", "o253p6U4rzqQhKVzPtWkONT1s7Sg");
+        Wx.sendTemplateMessage(MessageFactory.resolveMessage(girlFriend));
+
+       
+    }
+
+}
---
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -0,0 +1,39 @@
+package cn.ofpp;
+
+import cn.hutool.core.util.StrUtil;
+import cn.ofpp.core.Wx;
+
+/**
+ * 引导配置
+ *
+ * @author DokiYolo
+ * Date 2022-08-22
+ */
+public class Bootstrap {
+
+    /**
+     * 公众号AppID
+     */
+    public static final String APP_ID = "wx62e43bd6f94bd5f4";
+
+    /**
+     * 公众号秘钥
+     */
+    public static final String SECRET = "8c5a74d09850ad82553cd9013e21a0cc";
+
+    /**
+     * 全局模板ID  也可针对单个Friend指定模板
+     */
+    public static final String TEMPLATE_ID = "_vPENh3xNIwW-k-CcSKypn5dRn_BUapnKgNtQGHFiLw";
+
+    /**
+     * 初始化
+     */
+    public static void init() {
+        if (StrUtil.hasEmpty(APP_ID, SECRET, TEMPLATE_ID)) {
+            throw new IllegalArgumentException("请检查配置");
+        }
+        Wx.init();
+    }
+
+}
