71bb87a585039befaa2ce9946d9299a0c33bdef3
zhuchaojing <95963780+zhuchaojing@users.noreply.github.com>
2022-08-25 15:46:57 +0800

    Update Bootstrap.java

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -24,7 +24,7 @@ public class Bootstrap {
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "_vPENh3xNIwW-k-CcSKypn5dRn_BUapnKgNtQGHFiLw";
+    public static final String TEMPLATE_ID = "oG5VIhF09uKjEcdeMMWOxomgCeR4iYBRDivroRDY0Bs";
 
     /**
      * 初始化
