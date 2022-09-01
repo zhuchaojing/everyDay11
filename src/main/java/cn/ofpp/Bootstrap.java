6abd6c1aaa888063468d6f74e57028faac57b68b
朱朝靖(w_zhuchaojing) <396-w_zhuchaojing@users.noreply.10.2.38.240>
2022-09-01 11:30:45 +0800

    everyDay

--- src/main/java/cn/ofpp/Bootstrap.java
+++ src/main/java/cn/ofpp/Bootstrap.java
@@ -24,7 +24,7 @@ public class Bootstrap {
     /**
      * 全局模板ID  也可针对单个Friend指定模板
      */
-    public static final String TEMPLATE_ID = "aLSg5PJYiMzSR3hzRu3QfJqJH34OwPZjhmw5TMo17Cs";
+    public static final String TEMPLATE_ID = "3oT6RLNfMsCSRXl1eC6ocghwZ3TMIwDXNqE1aVCN4rg";
 
     /**
      * 初始化
